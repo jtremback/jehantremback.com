e68bf387b5bc30615c64c077eac0a97e3200e245
Jehan Tremback <jehan.tremback@gmail.com>
2013-04-04 00:13:03 -0700

    got frontend js really clean

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,78 +1,46 @@
-(function($) {
-  $.fn.pagify = function(prePath) {
-    var that = this;
 
-    that.switchPage = function() {
-      page = window.location.hash.replace('#','');
 
-      // Do not run if the hash is empty
-      if (window.location.hash != '') {
-        // Fetch page popup
-        $.get(prePath + page + '.html', function(popup) {
-          $(that).html(popup);
-        }, 'text');
-      }
+var popup = (function () {
+
+  var el = $('#popup'),
+      closer = $('#popup').find('.close'),
+      holder = $('#popup').find('#holder'),
+      prePath = 'content/';
+
+  return {
+    open : function(page) {
+      console.log("sto");
+      var urlFrag = page.replace('#','');
+
+      // Fetch page popup
+      $.get(prePath + urlFrag + '.html', function(content) {
+        holder.html(content);
+      }, 'text');
+
+      el.addClass('shown');
+    },
+
+    close : function() {
+      el.removeClass('shown');
     }
-
-    //Run it if the page already has a hash
-    if (window.location.hash) that.switchPage();
-
-    // Respond to hash changes
-    $(window).bind('hashchange', function() {
-      that.switchPage();
-    });    
-
   };
-})(jQuery);
 
-
-
-var popup = {
-  //store relevant dom elements
-  el : $('#popup'),
-  closer : $('#popup').find('.close'),
-  holder : $('#popup').find('#holder'),
-
-  open : function(defaultPage) {
-    window.location.hash = defaultPage;
-    popup.el.addClass('shown');
-  },
-
-  close : function() {
-    popup.el.removeClass('shown');
-    window.location.hash = '';
-  }
-}
-
-
-
-var polaroids = {
-  //store relevant dom elements
-  el : $('.wrapper'),
-  link : $('.polaroid').find('.link'),
-
-  getPage : function (event) {
-    //assign jq to useful var
-    var defaultPage = $(this).parents('.polaroid').attr('data-default-page');
-
-    popup.open(defaultPage);
-  }
-}
-
+})();
 
 
 var init = function() {
-  //turn on pep on all polaroids
-  polaroids.el.pep();
-  
-  //put pagify on popup area
-  popup.holder.pagify('content/');
+  //if window has a hash, run with it
+  var hashProc = function() {
+    if (window.location.hash) {
+      popup.open(window.location.hash);
+    } else {
+      popup.close()
+    }
+  } 
+  hashProc();
 
-  if (window.location.hash) {
-    popup.open();
-  }
-
-  //attach events
-  polaroids.link.on('mouseup', polaroids.getPage);
-  popup.closer.on('mouseup', popup.close);
+  //respond to hash changes w/out reloads
+  $(window).bind('hashchange', function() {
+    hashProc();
+  });
 }();
