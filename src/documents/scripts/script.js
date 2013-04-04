402674731b717f2129c94aaa973d9de843cdb168
Jehan Tremback <jehan.tremback@gmail.com>
2013-04-04 13:39:54 -0700

    working naive implementation with top class

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -31,33 +31,17 @@ var polaroid = (function() {
   return {
     wrapper: wrapper,
 
-    init : function() {
-      console.log('init');
-      //add classes for the visual and attach data to dom
-      $.each(stack, function(index) {
-        $(this).attr('id', index)
-      })
-    },
-
     bringToTop : function() {
       console.log('bbt');
-      //grab clicked element out of array based on order in dom
-      var order = $(this).attr('id'),
-          removed = stack.splice(order, 1);
-
-      //add back on to end of array
-      stack.push(removed)
-
-      //encode new correct order to dom 
-      polaroid.init();
+      wrapper.removeClass('top')
+      $(this).addClass('top');
     }
   }
 })();
 
 
 var init = function() {
-  // polaroid.init();
-  // $('.wrapper').on('mouseup', polaroid.bringToTop);
+  $('.wrapper').on('mouseup', polaroid.bringToTop);
   polaroid.wrapper.pep({ shouldPreventDefault: true });
   //if window has a hash, run with it
   var hashProc = function() {
